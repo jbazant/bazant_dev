3389326a7d3e85443f8444e76b3606c409f35b35
Jiri Bazant <jiri.bazant@memsource.com>
2020-01-02 22:31:17 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.glsl$/,
+                loader: 'webpack-glsl'
+            }
+        ],
+    },
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts' ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: false,
+        port: 9000
+    }
+};
