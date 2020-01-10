e384c1da7b5cdffb64ad5f442c9a4ca0e1665661
Jiri Bazant <jiri.bazant@memsource.com>
2020-01-10 11:17:54 +0100

    dependencies and project settings update

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
   printWidth: 100,
   tabWidth: 2,
 };
