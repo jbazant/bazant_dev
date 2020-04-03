41088962f33b352460ac7be4e68756bebf1b1796
Jiri Bazant <jiri.bazant@memsource.com>
2020-04-03 18:59:33 +0200

    initial commit

--- src/utils/debounce.ts
+++
@@ -1,8 +0,0 @@
-export function debounce<T>(func: (...args: T[]) => void, wait: number) {
-  let timeout: NodeJS.Timeout;
-
-  return (...args: T[]) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func(...args), wait);
-  };
-}
