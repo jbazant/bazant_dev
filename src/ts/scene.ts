1b6a0ad5213d11fa6494795087d6d37d5b97139c
Jiri Bazant <jiri.bazant@memsource.com>
2020-04-25 00:13:58 +0200

    header, footer and navigation

---
+++ src/ts/scene.ts
@@ -0,0 +1,27 @@
+import { SmartScene } from './webgl/SmartScene';
+import { getConfig } from './config';
+
+const canvas = document.getElementById('water-canvas') as HTMLCanvasElement;
+const gl = canvas && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'));
+
+if (gl && gl instanceof WebGLRenderingContext) {
+  const smartScene = new SmartScene(canvas, getConfig(gl.getSupportedExtensions()));
+  const getClassList = (name: string) =>
+    document.getElementById(`loading-${name}-placeholder`).classList;
+
+  let loadedSuccessfuly = true;
+  const onSuccess = () => {
+    if (loadedSuccessfuly) {
+      getClassList('info').add('no-vis');
+      smartScene.run();
+    }
+  };
+
+  const onFail = () => {
+    loadedSuccessfuly = false;
+    getClassList('info').add('no-vis');
+    getClassList('error').remove('no-vis');
+  };
+
+  smartScene.onReady(onSuccess, onFail);
+}
