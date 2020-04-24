4989d4a477a3f4f5960ff82830ccb58f8777e416
Jiri Bazant <jiri.bazant@memsource.com>
2020-04-25 00:13:58 +0200

    header, footer and navigation

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,31 +1,6 @@
-import { SmartScene } from './webgl/SmartScene';
-import { getConfig } from './config';
 import { getFamily } from './family';
+import { ready } from './helpers';
+import { initMenu } from './menu';
 
-const canvas = document.getElementById('water-canvas') as HTMLCanvasElement;
-const gl = canvas && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'));
-
-if (gl && gl instanceof WebGLRenderingContext) {
-  const smartScene = new SmartScene(canvas, getConfig(gl.getSupportedExtensions()));
-  const getClassList = (name: string) =>
-    document.getElementById(`loading-${name}-placeholder`).classList;
-
-  let loadedSuccessfuly = true;
-  const onSuccess = () => {
-    if (loadedSuccessfuly) {
-      getClassList('info').add('no-vis');
-      smartScene.run();
-    }
-  };
-
-  const onFail = () => {
-    loadedSuccessfuly = false;
-    getClassList('info').add('no-vis');
-    getClassList('error').remove('no-vis');
-  };
-
-  smartScene.onReady(onSuccess, onFail);
-}
-
-alert('it works!');
+ready(initMenu);
 export { getFamily };
---
+++ src/ts/menu.ts
@@ -0,0 +1,13 @@
+export function initMenu() {
+  const displayButton = document.getElementById('display-site-menu-main');
+  const closeButton = document.getElementById('close-site-menu-main');
+  const mainMenu = document.getElementById('site-menu-main');
+
+  displayButton.addEventListener('click', function () {
+    mainMenu.classList.add('m-fadeIn');
+  });
+
+  closeButton.addEventListener('click', function () {
+    mainMenu.classList.remove('m-fadeIn');
+  });
+}
