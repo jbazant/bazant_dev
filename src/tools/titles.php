5972a8e6db5d46c776e0c11c3aa6f39ac6dc157d
Jiri Bazant <jiri.bazant@memsource.com>
2021-10-11 09:08:00 +0200

    titles generator refactored

--- src/tools/titles.php
+++ src/tools/titles.php
@@ -5,30 +5,46 @@
  * Originally meant for my react-redux tutorial.
  */
 
-$dictionary = [
-        ['Rincewind', 'Lukeas Lus', 'Bilbo', 'Duncan', 'Hari', 'Zaphod', 'Kvothe'],
-        ['of', 'with', 'on', 'under', 'and'],
-        ['Frozen', 'Flame', 'Lame', 'Leafy', 'Dusty', 'Magic', 'Stone'],
-        ['River', 'Moon', 'Book', 'Wand', 'Boulder', 'Goblet', 'Pen', 'Knife', 'Fork']
-];
-
-function pickByRandom($dictionary) {
+function pickByRandom($dictionary): string {
     return $dictionary[array_rand($dictionary)];
 }
 
-$titleArr = [];
+function getScriptParam(string $name, $defaultValue = null) {
+    $val = $_SERVER['REQUEST_METHOD'] === 'POST' ? $_POST[$name] : $_GET[$name];
 
-if (($_POST && $_POST['prefix'])) {
-    array_push($titleArr, preg_replace('/[^\p{L}\p{N}]/u', ' ', $_POST['prefix']));
-}
-else {
-    array_push($titleArr, pickByRandom($dictionary[0]));
-}
+    if ($val) {
+        return preg_replace('/[^\p{L}\p{N}]/u', ' ', $val);
+    }
 
-for ($i = 1; $i < count($dictionary); ++$i) {
-    array_push($titleArr, pickByRandom($dictionary[$i]));
+    return $defaultValue;
 }
 
-sleep(3);
-echo json_encode(['title' => implode(' ', $titleArr)]);
+function generateTitle(?string $prefix): string {
+    $dictionary = [
+            ['Rincewind', 'Lukeas Lus', 'Bilbo', 'Duncan', 'Hari', 'Zaphod', 'Kvothe'],
+            ['of', 'with', 'on', 'under', 'and'],
+            ['Frozen', 'Flame', 'Lame', 'Leafy', 'Dusty', 'Magic', 'Stone'],
+            ['River', 'Moon', 'Book', 'Wand', 'Boulder', 'Goblet', 'Pen', 'Knife', 'Fork']
+    ];
 
+    $titleArr = [];
+
+    array_push($titleArr, $prefix ? $prefix : pickByRandom($dictionary[0]));
+
+    for ($i = 1; $i < count($dictionary); ++$i) {
+        array_push($titleArr, pickByRandom($dictionary[$i]));
+    }
+
+    return implode(' ', $titleArr);
+}
+
+function sendResponse(string $title, int $sleep): void {
+    sleep($sleep);
+    header('Content-Type: application/json');
+    echo json_encode(['title' => $title]);
+}
+
+$prefix = getScriptParam('prefix');
+$sleep = (int)getScriptParam('sleep', 3);
+
+sendResponse(generateTitle($prefix), $sleep);
