2af6ba0974bfc5a9a3f533f68247d7a13e390e9f
Jiří Bažant <j.bazant@gmail.com>
2022-05-04 15:41:52 +0200

    dlt update (#34)

--- src/tools/dlt.php
+++ src/tools/dlt.php
@@ -2,11 +2,15 @@
 /**
  * Deeplink tester.
  *
- * Tool that allows me to easily test one of my projects.
+ * Tool that allows me to easily test one of projects I participate.
  */
 
-$projectUid = $_GET['p'] ? urlencode(htmlspecialchars($_GET['p'])) : 'SOME_PROJECT_UID';
+$projectUid = array_key_exists('p', $_GET) ? urlencode(htmlspecialchars($_GET['p'])) : 'SOME_PROJECT_UID';
+
 $domains = ['qa', 'cloud9', 'cloud', 'us.cloud'];
+$selectedDomain = array_key_exists('c', $_GET) && in_array($_GET['c'], $domains) ? $_GET['c'] : $domains[0];
+$baseUrl = "https://$selectedDomain.memsource.com/web";
+
 $paths = [
     '/myProfile/index',
     '/project/list',
@@ -14,6 +18,7 @@ $paths = [
     '/project/create',
     '/job2/create/' . $projectUid
 ];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,27 +27,42 @@ $paths = [
     <title>Deep link test</title>
     <meta name="robots" content="noindex, nofollow"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="/css/main.css"/>
 </head>
 <body>
-<h1>Deep link tests</h1>
+<main>
+    <section class="basic">
+        <div class="container">
+            <h1>Deep link test</h1>
 
-<form method="get" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
-    <label for="project_uid">Project UID:</label>
-    <input name="p" type="text" value="<?php echo $projectUid ?>" id="project_uid" width="100"/>
-    <input type="submit" value="Set"/>
-</form>
+            <form method="get" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+                <div>
+                    <label for="project_uid">Project UID:</label>
+                    <input name="p" type="text" value="<?php echo $projectUid ?>" id="project_uid" width="100"/>
+                </div>
+                <div>
+                    <label for="domain">Domain:</label>
+                    <select name="c" id="domain">
+                        <?php foreach ($domains as $domain): ?>
+                            <option value="<?php echo $domain ?>"
+                                    <?php if ($domain === $selectedDomain): echo 'selected="selected"'; endif; ?>
+                            ><?php echo "$domain.memsource.com" ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <input type="submit" value="Set"  class="button-primary"/>
+            </form>
 
-<?php
-foreach ($domains as $domain):
-    $baseUrl = "https://$domain.memsource.com/web";
-?>
-    <h2><?php echo $domain ?></h2>
-    <p><?php echo $baseUrl ?></p>
-    <ul>
-        <?php foreach ($paths as $path): ?>
-            <li><a href="<?php print $baseUrl . $path ?>"><?php print $path ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-<?php endforeach; ?>
+            <h2><?php echo $selectedDomain ?></h2>
+            <p><?php echo $baseUrl ?></p>
+            <ul>
+                <?php foreach ($paths as $path): ?>
+                    <li><a href="<?php print $baseUrl . $path ?>"><?php print $path ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </section>
+</main>
 </body>
 </html>
